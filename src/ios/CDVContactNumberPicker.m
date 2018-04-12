ea8d77c4813ffe51c1176a262b88a34063c5e318
M A Hossain Tonu <mahtonu@vantage.com>
2018-04-12 16:55:08 +0600

    Refactored the code to eliminate obsolete functions.

--- src/ios/CDVContactNumberPicker.m
+++ src/ios/CDVContactNumberPicker.m
@@ -9,11 +9,11 @@
 
 - (void) pick:(CDVInvokedUrlCommand*)command{
     self.callbackID = command.callbackId;
-    
+
     // get contacts with phone number only
-    
+
 //    ABAddressBookRef addressBook = ABAddressBookCreate();
-//    
+//
 //    NSArray *contacts = CFBridgingRelease(ABAddressBookCopyArrayOfAllPeople(addressBook));
 //    for (id currentPerson in contacts) {
 //        ABRecordRef person = CFBridgingRetain(currentPerson);
@@ -25,32 +25,32 @@
 //        CFRelease(phones);
 //        CFRelease(person);
 //    }
-    
-    
+
+
     ABPeoplePickerNavigationController *picker = [[ABPeoplePickerNavigationController alloc] init];
     [picker setPeoplePickerDelegate:self];
-    
+
 //  [picker setAddressBook:addressBook];
 //  CFRelease(addressBook);
-    
+
     // display phone only
-    
+
     NSArray *phoneProp = [NSArray arrayWithObject:[NSNumber numberWithInt:kABPersonPhoneProperty]];
     picker.displayedProperties = phoneProp;
-    
+
     [self.viewController presentModalViewController:picker animated:YES];
-    
-    
+
+
 }
 
 - (NSMutableDictionary *)convertToDictionary:(ABRecordRef)person withPropertyID:(ABPropertyID)property withIdentifier:(ABMultiValueIdentifier)identifier {
-    
+
     NSString *name, *phone;
     name = (__bridge NSString*)ABRecordCopyCompositeName(person);
     if (!name) {
         name = @"";
     }
-    
+
     ABMultiValueRef mv = ABRecordCopyValue(person, property);
     if(ABMultiValueGetCount(mv) > 0){
         phone = (__bridge NSString *)ABMultiValueCopyValueAtIndex(mv, identifier);
@@ -58,24 +58,22 @@
     else{
         phone = @"";
     }
-    
+
     NSMutableDictionary* contact = [NSMutableDictionary dictionaryWithCapacity:2];
     [contact setObject:phone forKey: @"phoneNumber"];
     [contact setObject:name forKey: @"name"];
-    
+
     ABRecordID recordID = ABRecordGetRecordID(person);
     [contact setObject:@(recordID) forKey:@"id"];
-    
+
     return contact;
 }
 
 - (void)sendResult:(NSMutableDictionary *)contact {
-    
-    [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:contact] toSuccessCallbackString:self.callbackID]];
     [self.viewController dismissModalViewControllerAnimated:YES];
-    
+
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:contact];
-    
+
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackID];
 }
 
@@ -92,20 +90,16 @@
 
 - (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier
 {
-    
+
     NSMutableDictionary *contact;
     contact = [self convertToDictionary:person withPropertyID:property withIdentifier:identifier];
     [self sendResult:contact];
-    
+
     return NO;
 }
 
 - (void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker{
     [self.viewController dismissModalViewControllerAnimated:YES];
-    [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT]
-                            toErrorCallbackString:self.callbackID]];
 }
 
 @end
-
-
